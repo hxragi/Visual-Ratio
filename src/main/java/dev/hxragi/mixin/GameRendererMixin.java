bb16386233c870717869fb4720953c74fe8e8a33
hxragi <mixintrace@gmail.com>
2026-01-30 01:03:32 +0300

    refactor: optimize mixin

--- src/main/java/dev/hxragi/mixin/GameRendererMixin.java
+++ src/main/java/dev/hxragi/mixin/GameRendererMixin.java
@@ -1,6 +1,7 @@
 package dev.hxragi.mixin;
 
 import dev.hxragi.VisualRatio;
+import dev.hxragi.render.ProjectionCache;
 import net.minecraft.client.renderer.GameRenderer;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,16 +18,10 @@ public abstract class GameRendererMixin {
 
   @Inject(method = "getProjectionMatrix", at = @At("TAIL"), cancellable = true)
   private void visualratio$modifyProjectionMatrix(float fov, CallbackInfoReturnable<Matrix4f> cir) {
-    if (VisualRatio.getConfig().enable.get()) {
-      Matrix4f original = cir.getReturnValue();
-      Matrix4f modified = new Matrix4f(original);
+    if (!VisualRatio.getConfig().enable.get())
+      return;
 
-      modified.setPerspective(
-          (float) Math.toRadians(fov),
-          VisualRatio.getConfig().aspectRatio.get(),
-          original.m22(),
-          original.m23());
-      cir.setReturnValue(modified);
-    }
+    Matrix4f matrix = ProjectionCache.get(fov, VisualRatio.getConfig().aspectRatio.get(), this.renderDistance * 4);
+    cir.setReturnValue(matrix);
   }
 }
--- src/main/java/dev/hxragi/util/RatioManager.java
+++ src/main/java/dev/hxragi/util/RatioManager.java
@@ -1,6 +1,7 @@
 package dev.hxragi.util;
 
 import dev.hxragi.VisualRatio;
+import dev.hxragi.render.ProjectionCache;
 
 public final class RatioManager {
   public RatioManager() {
@@ -10,11 +11,13 @@ public final class RatioManager {
     var config = VisualRatio.config;
     config.aspectRatio
         .setAndUpdate(config.aspectRatio.get() + config.ratioStep.get());
+    ProjectionCache.invalidate();
   }
 
   public void decreaseRatio() {
     var config = VisualRatio.config;
     config.aspectRatio
         .setAndUpdate(config.aspectRatio.get() - config.ratioStep.get());
+    ProjectionCache.invalidate();
   }
 }
